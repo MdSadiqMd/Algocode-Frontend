5d93b619b86a937998813ade08f81978aa861586
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-21 18:55:09 +0530

    AlgoCode frontend

--- src/pages/Description/ProblemDescription.tsx
+++ src/pages/Description/ProblemDescription.tsx
@@ -38,11 +38,11 @@ function Description({ descriptionText }: {descriptionText: string}) {
         try {
             console.log(code)
             console.log(language)
-            const response = await axios.post("http://localhost:3000/api/v1/submissions", {
+            const response = await axios.post("http://localhost:8080/api/v1/submissions", {
                 code,
                 language,
                 userId: "1",
-                problemId: "6671b5e86f909d206b2d40a4"
+                problemId: "669cd25d2174227a4b9c6a4d"
             });
             console.log(response);
             return response;
